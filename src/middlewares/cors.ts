c64eb9873bd372080bd86245306d5778f85892db
zafkiel45 <zafkielc@gmail.com>
2025-03-14 18:22:06 -0300

    feat: CORS Middleware

---
+++ src/middlewares/cors.ts
@@ -0,0 +1,12 @@
+export function authCors(req: any, res: any, next: any) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if(req.method === "OPTION") {
+        res.sendStatus(204);
+    };
+
+    next();
+};
--- src/routes/test_route.ts
+++ src/routes/test_route.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { authCors } from '../middlewares/cors';
 export const testRoute = express.Router();
+
+testRoute.use(authCors);
 
 testRoute.get('/', (req, res) => {
     console.log(res.sendStatus(200));
