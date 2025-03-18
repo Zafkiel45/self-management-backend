2f36e56fd3bc11e6928c83f7f36a612384b9ea0a
zafkiel45 <zafkielc@gmail.com>
2025-03-18 20:45:19 -0300

    fix: removed uncessary comma

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -2,9 +2,19 @@ import { db } from "./connection";
 
 db.run(`
     CREATE TABLE IF NOT EXISTS users (
-      id INT PRIMARY KEY AUTOINCREMENT,
-      name CHAR NOT NULL
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name CHAR NOT NULL,
       password CHAR NOT NULL
     )
 `);
+
+/*
+    I did not why, but put in a comman in the final item of CREATE TABLE 
+    generate an error. 
+*/
+
+/*
+    I thought INT was parset to INTEGER, but this triggered an error. I do not
+    know if the problem is the Bun API or the SQlite.
+*/
 
