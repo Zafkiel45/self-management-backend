335b617be50a07b75185c73040b2388a8b495613
zafkiel45 <zafkielc@gmail.com>
2025-03-28 21:24:01 -0300

    feat: added more schemes

--- src/database/migrations.ts
+++ src/database/migrations.ts
@@ -1,5 +1,6 @@
 import { db } from "./connection";
 
+db.run(`PRAGMA foreign_keys = ON;`);
 db.run(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -7,6 +8,23 @@ db.run(`
       password CHAR NOT NULL
     )
 `);
+db.run(`
+    CREATE TABLE IF NOT EXISTS activities (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name CHAR NOT NULL UNIQUE, 
+        user_id INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    )    
+`);
+db.run(`
+    CREATE TABLE IF NOT EXISTS activities_log (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name CHAR NOT NULL UNIQUE, 
+        activity_id INTEGER,
+        FOREIGN KEY (activity_id) REFERENCES activities(id) ON DELETE CASCADE
+    )    
+`);
+
 
 /*
     I did not why, but put in a comma in the final item of CREATE TABLE 
